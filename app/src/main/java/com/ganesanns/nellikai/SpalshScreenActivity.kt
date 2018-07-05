83d4aae4a92e88f81538ac88ebb7c90e6ec21587
GanesanNS <Ganesan.NatarajanShanmugam@team.telstra.com>
2018-07-05 07:17:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/ganesanns/nellikai/SpalshScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.ganesanns.nellikai
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SpalshScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh_screen)
+    }
+}
